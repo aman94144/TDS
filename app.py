c0826efc9b5bc19d599c2ab8a5b4c51ca714d0ed
Aniket Aman <68724304+aman94144@users.noreply.github.com>
2023-08-31 21:29:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ def find_largest(num1, num2, num3):
 def main():
     st.title("Find the Largest Number")
     
+    st.write("Enter three numbers and click the button to find the largest:")
+    
     num1 = st.number_input("Enter the first number:")
     num2 = st.number_input("Enter the second number:")
     num3 = st.number_input("Enter the third number:")
     
     if st.button("Find Largest"):
         largest = find_largest(num1, num2, num3)
-        st.write(f"The largest number is: {largest}")
+        st.write(f"The largest number is: **{largest}**")
+        st.balloons()  # Add a fun effect when the largest number is displayed
 
 if __name__ == "__main__":
     main()
